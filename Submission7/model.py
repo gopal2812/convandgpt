455adce75a9c702b5174add630cbb95d86a30150
gopal2812 <39087216+gopal2812@users.noreply.github.com>
2024-03-15 23:25:14 +0530

    Update model.py

--- Submission7/model.py
+++ Submission7/model.py
@@ -10,39 +10,39 @@ class Model_1(nn.Module):
         super(Model_1, self).__init__()
 
         self.conv1 = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=3),
+            nn.Conv2d(1, 20, kernel_size=3),
             nn.ReLU(),
-            nn.BatchNorm2d(16),
-            nn.Conv2d(16, 16, kernel_size=3),
+            nn.BatchNorm2d(20),
+            nn.Conv2d(20, 10, kernel_size=3),
             nn.ReLU(),
-            nn.BatchNorm2d(16),
-            nn.Conv2d(16, 32, kernel_size=3),
+            nn.BatchNorm2d(10),
+            nn.Conv2d(10, 11, kernel_size=3),
             nn.ReLU(),
-            nn.BatchNorm2d(32),
+            nn.BatchNorm2d(11),
         )
         self.conv2 = nn.Sequential(
-            nn.Conv2d(32, 32, kernel_size=1, stride=2),
-            # nn.ReLU(),
-            # nn.BatchNorm2d(32),
-            nn.Conv2d(32, 16, kernel_size=3),
+            nn.Conv2d(11, 10, kernel_size=1),
             nn.ReLU(),
-            nn.BatchNorm2d(16),
-            nn.Conv2d(16, 16, kernel_size=3),
+            nn.BatchNorm2d(10),
+            nn.Conv2d(10, 10, kernel_size=3),
             nn.ReLU(),
-            nn.BatchNorm2d(16),
-            nn.Conv2d(16, 32, kernel_size=3),
+            nn.BatchNorm2d(10),
+            nn.Conv2d(10, 10, kernel_size=3),
             nn.ReLU(),
-            nn.BatchNorm2d(32),
+            nn.BatchNorm2d(10),
+            nn.Conv2d(10, 22, kernel_size=3),
+            nn.ReLU(),
+            nn.BatchNorm2d(22),
+            nn.Conv2d(22, 10, kernel_size=1),
         )
-        self.avgpool = nn.AvgPool2d(5)
-        self.fc1 = nn.Linear(32, 10)
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
 
     def forward(self, x):
         x = self.conv1(x)
+        x = F.max_pool2d(x, 2)
         x = self.conv2(x)
         x = self.avgpool(x)
-        x = x.view(-1, 32)
-        x = self.fc1(x)
+        x = x.view(-1, 10)
         return F.log_softmax(x, dim=1)
 
 
