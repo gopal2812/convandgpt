d577a51bc53125ca7f87a3a28720f66dbe46fad5
gopal2812 <39087216+gopal2812@users.noreply.github.com>
2024-03-16 08:51:47 +0530

    Update model.py

--- Submission5/model.py
+++ Submission5/model.py
@@ -12,15 +12,15 @@ class Net(nn.Module):
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
         self.conv4 = nn.Conv2d(128, 256, kernel_size=3)
-        self.fc1 = nn.Linear(320, 50)
+        self.fc1 = nn.Linear(4096, 50)
         self.fc2 = nn.Linear(50, 10)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x), 2)
-        x = F.relu(F.max_pool2d(self.conv2(x), 2))
-        x = F.relu(self.conv3(x), 2)
-        x = F.relu(F.max_pool2d(self.conv4(x), 2))
-        x = x.view(-1, 320)
+        x = F.relu(self.conv1(x), 2) # 28>26 | 1>3 | 1>1
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))  # 26>24>12 | 3>5>6 | 1>1>2
+        x = F.relu(self.conv3(x), 2)   # 12>10 | 6>10 | 2>2
+        x = F.relu(F.max_pool2d(self.conv4(x), 2))   # 10>8>4 | 10>14>16 | 2>2>4
+        x = x.view(-1, 4096)             # 4*4*256
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
